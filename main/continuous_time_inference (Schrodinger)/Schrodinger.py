5e7eb408901e02f4b5c83ba9547426c3b7f52da0
Árpád Rózsás <arpad.rozsas@tno.nl>
2019-02-16 21:00:29 +0100

    improved PEP8 compliance

--- main/continuous_time_inference (Schrodinger)/Schrodinger.py
+++ main/continuous_time_inference (Schrodinger)/Schrodinger.py
@@ -3,7 +3,6 @@
 """
 
 import sys
-sys.path.insert(0, '../../Utilities/')
 
 import tensorflow as tf
 import numpy as np
@@ -12,11 +11,12 @@ import scipy.io
 from scipy.interpolate import griddata
 from pyDOE import lhs
 from plotting import newfig, savefig
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 import time
 import matplotlib.gridspec as gridspec
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+sys.path.insert(0, '../../Utilities/')
 
 np.random.seed(1234)
 tf.set_random_seed(1234)
@@ -26,24 +26,24 @@ class PhysicsInformedNN:
     # Initialize the class
     def __init__(self, x0, u0, v0, tb, X_f, layers, lb, ub):
         
-        X0 = np.concatenate((x0, 0*x0), 1) # (x0, 0)
-        X_lb = np.concatenate((0*tb + lb[0], tb), 1) # (lb[0], tb)
-        X_ub = np.concatenate((0*tb + ub[0], tb), 1) # (ub[0], tb)
+        X0 = np.concatenate((x0, 0*x0), 1)  # (x0, 0)
+        X_lb = np.concatenate((0*tb + lb[0], tb), 1)  # (lb[0], tb)
+        X_ub = np.concatenate((0*tb + ub[0], tb), 1)  # (ub[0], tb)
         
         self.lb = lb
         self.ub = ub
                
-        self.x0 = X0[:,0:1]
-        self.t0 = X0[:,1:2]
+        self.x0 = X0[:, 0:1]
+        self.t0 = X0[:, 1:2]
 
-        self.x_lb = X_lb[:,0:1]
-        self.t_lb = X_lb[:,1:2]
+        self.x_lb = X_lb[:, 0:1]
+        self.t_lb = X_lb[:, 1:2]
 
-        self.x_ub = X_ub[:,0:1]
-        self.t_ub = X_ub[:,1:2]
+        self.x_ub = X_ub[:, 0:1]
+        self.t_ub = X_ub[:, 1:2]
         
-        self.x_f = X_f[:,0:1]
-        self.t_f = X_f[:,1:2]
+        self.x_f = X_f[:, 0:1]
+        self.t_f = X_f[:, 1:2]
         
         self.u0 = u0
         self.v0 = v0
@@ -69,7 +69,7 @@ class PhysicsInformedNN:
         self.t_f_tf = tf.placeholder(tf.float32, shape=[None, self.t_f.shape[1]])
 
         # tf Graphs
-        self.u0_pred, self.v0_pred, _ , _ = self.net_uv(self.x0_tf, self.t0_tf)
+        self.u0_pred, self.v0_pred, _, _ = self.net_uv(self.x0_tf, self.t0_tf)
         self.u_lb_pred, self.v_lb_pred, self.u_x_lb_pred, self.v_x_lb_pred = self.net_uv(self.x_lb_tf, self.t_lb_tf)
         self.u_ub_pred, self.v_ub_pred, self.u_x_ub_pred, self.v_x_ub_pred = self.net_uv(self.x_ub_tf, self.t_ub_tf)
         self.f_u_pred, self.f_v_pred = self.net_f_uv(self.x_f_tf, self.t_f_tf)
@@ -86,12 +86,12 @@ class PhysicsInformedNN:
         
         # Optimizers
         self.optimizer = tf.contrib.opt.ScipyOptimizerInterface(self.loss, 
-                                                                method = 'L-BFGS-B', 
-                                                                options = {'maxiter': 50000,
-                                                                           'maxfun': 50000,
-                                                                           'maxcor': 50,
-                                                                           'maxls': 50,
-                                                                           'ftol' : 1.0 * np.finfo(float).eps})
+                                                                method='L-BFGS-B',
+                                                                options={'maxiter': 50000,
+                                                                         'maxfun': 50000,
+                                                                         'maxcor': 50,
+                                                                         'maxls': 50,
+                                                                         'ftol': 1.0 * np.finfo(float).eps})
     
         self.optimizer_Adam = tf.train.AdamOptimizer()
         self.train_op_Adam = self.optimizer_Adam.minimize(self.loss)
@@ -124,7 +124,7 @@ class PhysicsInformedNN:
         num_layers = len(weights) + 1
         
         H = 2.0*(X - self.lb)/(self.ub - self.lb) - 1.0
-        for l in range(0,num_layers-2):
+        for l in range(0, num_layers-2):
             W = weights[l]
             b = biases[l]
             H = tf.tanh(tf.add(tf.matmul(H, W), b))
@@ -134,11 +134,11 @@ class PhysicsInformedNN:
         return Y
     
     def net_uv(self, x, t):
-        X = tf.concat([x,t],1)
+        X = tf.concat([x, t], 1)
         
         uv = self.neural_net(X, self.weights, self.biases)
-        u = uv[:,0:1]
-        v = uv[:,1:2]
+        u = uv[:, 0:1]
+        v = uv[:, 1:2]
         
         u_x = tf.gradients(u, x)[0]
         v_x = tf.gradients(v, x)[0]
@@ -146,7 +146,7 @@ class PhysicsInformedNN:
         return u, v, u_x, v_x
 
     def net_f_uv(self, x, t):
-        u, v, u_x, v_x = self.net_uv(x,t)
+        u, v, u_x, v_x = self.net_uv(x, t)
         
         u_t = tf.gradients(u, t)[0]
         u_xx = tf.gradients(u_x, x)[0]
@@ -183,20 +183,19 @@ class PhysicsInformedNN:
                 start_time = time.time()
                                                                                                                           
         self.optimizer.minimize(self.sess, 
-                                feed_dict = tf_dict,         
-                                fetches = [self.loss], 
-                                loss_callback = self.callback)        
+                                feed_dict=tf_dict,
+                                fetches=[self.loss],
+                                loss_callback=self.callback)
                                     
     
     def predict(self, X_star):
         
-        tf_dict = {self.x0_tf: X_star[:,0:1], self.t0_tf: X_star[:,1:2]}
+        tf_dict = {self.x0_tf: X_star[:, 0:1], self.t0_tf: X_star[:, 1:2]}
         
         u_star = self.sess.run(self.u0_pred, tf_dict)  
         v_star = self.sess.run(self.v0_pred, tf_dict)  
-        
-        
-        tf_dict = {self.x_f_tf: X_star[:,0:1], self.t_f_tf: X_star[:,1:2]}
+
+        tf_dict = {self.x_f_tf: X_star[:, 0:1], self.t_f_tf: X_star[:, 1:2]}
         
         f_u_star = self.sess.run(self.f_u_pred, tf_dict)
         f_v_star = self.sess.run(self.f_v_pred, tf_dict)
@@ -218,67 +217,63 @@ if __name__ == "__main__":
         
     data = scipy.io.loadmat('../Data/NLS.mat')
     
-    t = data['tt'].flatten()[:,None]
-    x = data['x'].flatten()[:,None]
+    t = data['tt'].flatten()[:, None]
+    x = data['x'].flatten()[:, None]
     Exact = data['uu']
     Exact_u = np.real(Exact)
     Exact_v = np.imag(Exact)
     Exact_h = np.sqrt(Exact_u**2 + Exact_v**2)
     
-    X, T = np.meshgrid(x,t)
+    X, T = np.meshgrid(x, t)
     
-    X_star = np.hstack((X.flatten()[:,None], T.flatten()[:,None]))
-    u_star = Exact_u.T.flatten()[:,None]
-    v_star = Exact_v.T.flatten()[:,None]
-    h_star = Exact_h.T.flatten()[:,None]
+    X_star = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))
+    u_star = Exact_u.T.flatten()[:, None]
+    v_star = Exact_v.T.flatten()[:, None]
+    h_star = Exact_h.T.flatten()[:, None]
     
     ###########################
     
     idx_x = np.random.choice(x.shape[0], N0, replace=False)
-    x0 = x[idx_x,:]
-    u0 = Exact_u[idx_x,0:1]
-    v0 = Exact_v[idx_x,0:1]
+    x0 = x[idx_x, :]
+    u0 = Exact_u[idx_x, 0:1]
+    v0 = Exact_v[idx_x, 0:1]
     
     idx_t = np.random.choice(t.shape[0], N_b, replace=False)
-    tb = t[idx_t,:]
+    tb = t[idx_t, :]
     
     X_f = lb + (ub-lb)*lhs(2, N_f)
             
     model = PhysicsInformedNN(x0, u0, v0, tb, X_f, layers, lb, ub)
              
     start_time = time.time()                
-    model.train(50000)
+    model.train(500)
     elapsed = time.time() - start_time                
-    print('Training time: %.4f' % (elapsed))
-    
-        
+    print('Training time: %.4f' % elapsed)
+
     u_pred, v_pred, f_u_pred, f_v_pred = model.predict(X_star)
     h_pred = np.sqrt(u_pred**2 + v_pred**2)
             
-    error_u = np.linalg.norm(u_star-u_pred,2)/np.linalg.norm(u_star,2)
-    error_v = np.linalg.norm(v_star-v_pred,2)/np.linalg.norm(v_star,2)
-    error_h = np.linalg.norm(h_star-h_pred,2)/np.linalg.norm(h_star,2)
-    print('Error u: %e' % (error_u))
-    print('Error v: %e' % (error_v))
-    print('Error h: %e' % (error_h))
+    error_u = np.linalg.norm(u_star-u_pred, 2)/np.linalg.norm(u_star, 2)
+    error_v = np.linalg.norm(v_star-v_pred, 2)/np.linalg.norm(v_star, 2)
+    error_h = np.linalg.norm(h_star-h_pred, 2)/np.linalg.norm(h_star, 2)
+    print('Error u: %e' % error_u)
+    print('Error v: %e' % error_v)
+    print('Error h: %e' % error_h)
 
-    
     U_pred = griddata(X_star, u_pred.flatten(), (X, T), method='cubic')
     V_pred = griddata(X_star, v_pred.flatten(), (X, T), method='cubic')
     H_pred = griddata(X_star, h_pred.flatten(), (X, T), method='cubic')
 
     FU_pred = griddata(X_star, f_u_pred.flatten(), (X, T), method='cubic')
     FV_pred = griddata(X_star, f_v_pred.flatten(), (X, T), method='cubic')     
-    
 
-    
     ######################################################################
     ############################# Plotting ###############################
     ######################################################################    
     
-    X0 = np.concatenate((x0, 0*x0), 1) # (x0, 0)
-    X_lb = np.concatenate((0*tb + lb[0], tb), 1) # (lb[0], tb)
-    X_ub = np.concatenate((0*tb + ub[0], tb), 1) # (ub[0], tb)
+    X0 = np.concatenate((x0, 0*x0), 1)  # (x0, 0)
+    X_lb = np.concatenate((0*tb + lb[0], tb), 1)  # (lb[0], tb)
+    X_ub = np.concatenate((0*tb + ub[0], tb), 1)  # (ub[0], tb)
     X_u_train = np.vstack([X0, X_lb, X_ub])
 
     fig, ax = newfig(1.0, 0.9)
@@ -296,53 +291,53 @@ if __name__ == "__main__":
     cax = divider.append_axes("right", size="5%", pad=0.05)
     fig.colorbar(h, cax=cax)
     
-    ax.plot(X_u_train[:,1], X_u_train[:,0], 'kx', label = 'Data (%d points)' % (X_u_train.shape[0]), markersize = 4, clip_on = False)
+    ax.plot(X_u_train[:, 1], X_u_train[:, 0], 'kx', label='Data (%d points)' % (X_u_train.shape[0]), markersize=4, clip_on=False)
     
-    line = np.linspace(x.min(), x.max(), 2)[:,None]
-    ax.plot(t[75]*np.ones((2,1)), line, 'k--', linewidth = 1)
-    ax.plot(t[100]*np.ones((2,1)), line, 'k--', linewidth = 1)
-    ax.plot(t[125]*np.ones((2,1)), line, 'k--', linewidth = 1)    
+    line = np.linspace(x.min(), x.max(), 2)[:, None]
+    ax.plot(t[75]*np.ones((2, 1)), line, 'k--', linewidth=1)
+    ax.plot(t[100]*np.ones((2,1)), line, 'k--', linewidth=1)
+    ax.plot(t[125]*np.ones((2,1)), line, 'k--', linewidth=1)
     
     ax.set_xlabel('$t$')
     ax.set_ylabel('$x$')
-    leg = ax.legend(frameon=False, loc = 'best')
+    leg = ax.legend(frameon=False, loc='best')
 #    plt.setp(leg.get_texts(), color='w')
-    ax.set_title('$|h(t,x)|$', fontsize = 10)
+    ax.set_title('$|h(t,x)|$', fontsize=10)
     
     ####### Row 1: h(t,x) slices ##################    
     gs1 = gridspec.GridSpec(1, 3)
     gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
     
     ax = plt.subplot(gs1[0, 0])
-    ax.plot(x,Exact_h[:,75], 'b-', linewidth = 2, label = 'Exact')       
-    ax.plot(x,H_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.plot(x, Exact_h[:, 75], 'b-', linewidth=2, label='Exact')
+    ax.plot(x, H_pred[75, :], 'r--', linewidth=2, label='Prediction')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$|h(t,x)|$')    
     ax.set_title('$t = %.2f$' % (t[75]), fontsize = 10)
     ax.axis('square')
-    ax.set_xlim([-5.1,5.1])
-    ax.set_ylim([-0.1,5.1])
+    ax.set_xlim([-5.1, 5.1])
+    ax.set_ylim([-0.1, 5.1])
     
     ax = plt.subplot(gs1[0, 1])
-    ax.plot(x,Exact_h[:,100], 'b-', linewidth = 2, label = 'Exact')       
-    ax.plot(x,H_pred[100,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.plot(x, Exact_h[:, 100], 'b-', linewidth=2, label='Exact')
+    ax.plot(x, H_pred[100, :], 'r--', linewidth=2, label='Prediction')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$|h(t,x)|$')
     ax.axis('square')
-    ax.set_xlim([-5.1,5.1])
-    ax.set_ylim([-0.1,5.1])
-    ax.set_title('$t = %.2f$' % (t[100]), fontsize = 10)
+    ax.set_xlim([-5.1, 5.1])
+    ax.set_ylim([-0.1, .1])
+    ax.set_title('$t = %.2f$' % (t[100]), fontsize=10)
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.8), ncol=5, frameon=False)
     
     ax = plt.subplot(gs1[0, 2])
-    ax.plot(x,Exact_h[:,125], 'b-', linewidth = 2, label = 'Exact')       
-    ax.plot(x,H_pred[125,:], 'r--', linewidth = 2, label = 'Prediction')
+    ax.plot(x, Exact_h[:, 125], 'b-', linewidth=2, label='Exact')
+    ax.plot(x, H_pred[125, :], 'r--', linewidth=2, label='Prediction')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$|h(t,x)|$')
     ax.axis('square')
-    ax.set_xlim([-5.1,5.1])
-    ax.set_ylim([-0.1,5.1])    
-    ax.set_title('$t = %.2f$' % (t[125]), fontsize = 10)
+    ax.set_xlim([-5.1, 5.1])
+    ax.set_ylim([-0.1, 5.1])
+    ax.set_title('$t = %.2f$' % (t[125]), fontsize=10)
     
     # savefig('./figures/NLS')  
     
